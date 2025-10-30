bcf96b0175ceea635a21fec9ce1c8ecec91b0c00
Hugo Mell <hugo76m@hotmail.com>
2025-10-30 18:21:22 +0100

    Quarto report as output of targets pipeline

---
+++ _targets.R
@@ -0,0 +1,16 @@
+# Load packages required to define the pipeline:
+library(targets)
+library(tarchetypes) # Load other packages as needed.
+
+# Define the target list with the pipeline steps:
+list(
+  tar_target(
+    name = data,
+    command = data.frame(x = seq_len(26), y = letters)
+  ),
+  # Compile quarto report
+  tar_quarto(
+    name = report,
+    path = "report.qmd"
+  )
+)
